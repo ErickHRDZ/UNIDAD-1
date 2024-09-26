39dc60bee136330cda4a7300540e40ee25fadec9
ErickHRDZ <erickantonior250@gmail.com>
2024-09-26 12:01:24 -0600

    Add files via upload

---
+++ Erick.cpp
@@ -0,0 +1,43 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main ()
+{
+	//mensaje de bienvenida
+	cout << "hola! este programa 1.6 escribe los datos en orden inverso" << "\n"
+	
+	//se declaran los numeros que se sumaran (pueden ser decimales)
+    int A,B,C,D;
+	
+	//se pide el primero numero
+	cout <<"por favor ingrese el primer valor A: "<< "\n";
+	
+	//se asigna el primer valor a A
+	cin >> A;
+	
+	//se pide el segundo numero
+	cout <<"por favor ingrese el segundo valor B: "<< "\n";
+	
+	//se asigna el segundo valor a B
+	cin >> B;	
+	
+		//se pide el tercer numero
+	cout <<"por favor ingrese el tercer valor C: "<< "\n";
+	
+	//se asigna el segundo valor a C
+	cin >> C;
+	
+		//se pide el tercer numero
+	cout <<"por favor ingrese el tercer valor D: "<< "\n";
+	
+	//se asigna el segundo valor a D
+	cin >> D;
+	
+	//se muestra el resultado.
+	cout << D >> " , " << C >> " , " << B >> " , " << A;
+	
+	return 0;
+	
+}
