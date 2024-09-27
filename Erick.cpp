682f6912b39877c31c5171d76d81c7df0a6b9a52
ErickHRDZ <erickantonior250@gmail.com>
2024-09-27 06:56:18 -0500

    Update Erick.cpp

--- Erick.cpp
+++ Erick.cpp
@@ -9,28 +9,28 @@ int main ()
 	cout << "hola! este programa 1.6 escribe los datos en orden inverso" << "\n"
 	
 	//se declaran los numeros que se sumaran (pueden ser decimales)
-    int A,B,C,D;
+        int A,B,C,D;
 	
 	//se pide el primero numero
-	cout <<"por favor ingrese el primer valor A: "<< "\n";
+	cout << "por favor ingrese el primer valor A: " << "\n";
 	
 	//se asigna el primer valor a A
 	cin >> A;
 	
 	//se pide el segundo numero
-	cout <<"por favor ingrese el segundo valor B: "<< "\n";
+	cout << "por favor ingrese el segundo valor B: " << "\n";
 	
 	//se asigna el segundo valor a B
 	cin >> B;	
 	
 		//se pide el tercer numero
-	cout <<"por favor ingrese el tercer valor C: "<< "\n";
+	cout << "por favor ingrese el tercer valor C: " << "\n";
 	
 	//se asigna el segundo valor a C
 	cin >> C;
 	
 		//se pide el tercer numero
-	cout <<"por favor ingrese el tercer valor D: "<< "\n";
+	cout << "por favor ingrese el tercer valor D: " << "\n";
 	
 	//se asigna el segundo valor a D
 	cin >> D;
