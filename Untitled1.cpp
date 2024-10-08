3268eaedd9f2f4d665099bcdf70962b9ca082ac2
ErickHRDZ <erickantonior250@gmail.com>
2024-10-08 12:02:59 -0600

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	float Altura,Base;
+	float SUPERFICIE, PERIMETRO;
+	//MENSAJE DE BIENVENIDA
+	cout <<"Hola! Este programa 1.10 Calcula la superficie y el perimetro de un rectangulo" <<"\n";
+	
+	//Se declaran los números que se sumarán (pueden ser decimales)
+	
+	//Se pide el primer número
+	cout<< "Por favor ingrese el valor de la Base:" << "\n";
+	//Se pide el primer valor a Base
+	cin >> Base;
+	//Se pide el segundo número
+	cout<< "Por favor ingrese el valor de la Altura:" << "\n";
+	//Se pide el segundo valor a Altura
+	cin >> Altura;
+	
+	SUPERFICIE= Base*Altura;
+	PERIMETRO=2* (Base+Altura);
+	
+	//Se muestra el resultado
+	printf ("\n La Superficie del rectangulo es %5.2f \n", SUPERFICIE);
+	printf ("\n El perimetro del rectangulo es %5.2f \n", SUPERFICIE);
+	
+	return 0;
+}
